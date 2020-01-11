9a655eeb9610ebb9f45272499430c0ab8f616506
Jam Risser <jam@codejam.ninja>
2020-01-11 22:34:40 +0000

    Added example

--- example/index.ts
+++ example/index.ts
@@ -7,3 +7,15 @@ console.log('netload (lo)', libGTop.getNetload('lo'));
 console.log('procArgs (1)', libGTop.getProcArgs(1));
 console.log('proclist', libGTop.proclist);
 console.log('uptime', libGTop.uptime);
+
+function getNodeProcesses(): number[] {
+  const libGTop = new LibGTop();
+  const pids: number[] = [];
+  libGTop.proclist.forEach((pid: number) => {
+    const args = libGTop.getProcArgs(pid);
+    if (args.indexOf('node') > -1) pids.push(pid);
+  });
+  return pids;
+}
+
+console.log('Node Processes', getNodeProcesses());
